671d6a6286a5c787cde68d119fab32d7bd7431eb
Viraj Navkal <vnavkal0@gmail.com>
2017-01-09 18:45:34 -0800

    some updates

--- digit_recognition.py
+++ digit_recognition.py
@@ -18,16 +18,10 @@ def fetch_data():
     return fetch_mldata('MNIST original', data_home=DATA_DIR)
 
 
-def train_knn(X, y, n_neighbors):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
-    model = NearestNeighbors(n_neighbors=n_neighbors).fit(X_train)
-    preds = model.kneighbors(X_test)
-
-
 def train_linear():
     sample = np.random.randint(0, len(X), 5000)
     X_sample = X[sample,:]
-    X_hog = np.array([my_hog(reshape_vector(x)) for x in X_sample])
+    X_hog = np.array([my_hog(reshape_image_vector(x)) for x in X_sample])
     y_sample = y[sample]
     X_train, X_test, y_train, y_test = (
         train_test_split(X_hog, y_sample, test_size=0.1, random_state=100)
@@ -71,7 +65,7 @@ def train_all_hogs(sample_size=10000, random_state=0):
 
 def train_single_hog(X_sample, y_sample, num_orientations, ppc, random_state, C):
     hog_features = np.array([
-        get_hog(reshape_vector(x), num_orientations, ppc)
+        get_hog(reshape_image_vector(x), num_orientations, ppc)
         for x in X_sample
     ])
     X_train, X_test, y_train, y_test = (
@@ -89,13 +83,13 @@ def take_sample(X, y, sample_size):
 
 
 def visualize_coef(coef, num_orientations, ppc):
-    reshaped = coef.reshape(num_orientations, 28 // ppc[1], 28 // ppc[0])
+    reshaped = reshape_hog_vector(coef, num_orientations, ppc)
     dim = math.ceil(num_orientations / num_orientations**.5)
     fig, axes = plt.subplots(nrows=dim, ncols=dim, figsize=(6, 6))
     for x in range(dim):
         for y in range(dim):
             if x * dim + y < num_orientations:
-                axes[x, y].imshow(reshaped[x * dim + y], cmap='gray')
+                axes[x, y].imshow(reshaped[:, :, x * dim + y], cmap='gray')
     # normalized = 256 * reshaped / reshaped.max()
     # plt.imshow(reshaped[0,:,:], cmap='gray')
     plt.show()
@@ -106,34 +100,43 @@ def aggregate_coef(all_coef, num_orientations, ppc):
     final = {}
     for digit in range(10):
         coef = all_coef[digit,:]
-        reshaped = coef.reshape(num_orientations, 28 // ppc[1], 28 // ppc[0])
+        reshaped = reshape_hog_vector(coef, num_orientations, ppc)
         final[digit] = {
-            orientation: np.abs(reshaped[orientation,:,:]).mean() for orientation in range(num_orientations)
+            orientation: np.abs(reshaped[:,:, orientation]).mean() for orientation in range(num_orientations)
         }
     return pd.DataFrame(final)
 
 
-def reshape_vector(v):
+def reshape_image_vector(v):
     return np.reshape(v, [28,28])
 
 
+def reshape_hog_vector(hog_vector, num_orientations, ppc):
+    return hog_vector.reshape(28 // ppc[1], 28 // ppc[0], num_orientations)
+
+
 def plot_vector(v):
-    reshaped = reshape_vector(v)
+    reshaped = reshape_image_vector(v)
     plt.imshow(reshaped, cmap='gray')
     plt.show()
 
 
-def get_hog(image, num_orientations, pixels_per_cell):
+def get_hog(image, num_orientations, pixels_per_cell, feature_vector=True):
     return hog(image,
                orientations=num_orientations,
                pixels_per_cell=pixels_per_cell,
                cells_per_block=(1, 1),
                visualise=False,
-               feature_vector=False)
-
+               feature_vector=feature_vector)
 
 
 if '__name__' == 'main':
     bunch = fetch_data()
     X = bunch.data
     y = bunch.target
+    X_sample, y_sample = take_sample(X, y, 10000)
+    num_orientations = 4
+    ppc = (3, 3)
+    random_state = 0
+    C = .05
+    model, score = train_single_hog(X_sample, y_sample, num_orientations, ppc, random_state, C)
